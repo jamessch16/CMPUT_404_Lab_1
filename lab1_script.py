a29ac3ae8910d0da8c1b727d7f100a6f57e75ceb
jamessch16 <jamessch16@gmail.com>
2023-09-11 18:13:38 -0600

    final question

--- lab1_script.py
+++ lab1_script.py
@@ -1,4 +1,3 @@
 import requests
-print(requests.__version__)
-r = requests.get("http://google.com/")
+r = requests.get("https://raw.githubusercontent.com/jamessch16/CMPUT_404_Lab_1/master/lab1_script.py")
 print(r.text)
